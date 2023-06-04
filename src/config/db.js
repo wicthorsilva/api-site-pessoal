a2d7b7941ef67a62ce6b7ef5f1ac1ffcf3d16786
wicthorsilva <luizwicthor@gmail.com>
2023-06-04 10:45:44 -0300

    Aplicando rejectUnauthorized

--- src/config/db.js
+++ src/config/db.js
@@ -6,7 +6,7 @@ const pool = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    ssl: process.env.NODE_ENV === 'production' ? true : false
+    ssl: process.env.NODE_ENV === 'production' ? {rejectUnauthorized:false} : false
 });
 
 const initDatabase = async () => {
