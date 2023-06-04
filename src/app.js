4ba7d9878f091ba300e8772aec400fa2969968bf
wicthorsilva <luizwicthor@gmail.com>
2023-06-04 19:13:54 -0300

    added error handler

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 require('./config/dotenv');
+require('express-async-errors')
 
 const express = require('express');
 const { initDatabase} = require('./config/db');
@@ -27,6 +28,11 @@ app.use('/api/informacoes', informacoesRoute);
 app.use('/api/auth', authRoute);
 
 initDatabase();
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send({'Erro': err.message});
+});
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
